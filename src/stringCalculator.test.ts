1e2db18d52738ed176adc43d37bc2378fad5264f
rishabjn <rishabjn2056@gmail.com>
2025-02-24 18:39:23 +0530

    Inital Repository setup + Basic add functionality with TDD

---
+++ src/stringCalculator.test.ts
@@ -0,0 +1,7 @@
+import { add } from "./stringCalculator";
+
+
+
+test("Test 1: adds single number", () => {
+    expect(add("1")).toBe(1);
+  });
---
+++ src/stringCalculator.ts
@@ -0,0 +1,14 @@
+
+/*
+* Input: a string of comma-separated numbers
+* Output: an integer, sum of the numbers
+*/
+
+export function add(numbers: string): number {
+    const addResult = numbers;
+    console.log("Welcome to Incubyte TDD Assessment! ");
+    
+    return Number(addResult);
+}
+
+add("1")
